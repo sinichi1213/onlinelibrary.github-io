76e51d48fead529ccfddb905231c39df24656ead
sinichi1213 <104555634+sinichi1213@users.noreply.github.com>
2023-02-15 20:29:48 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="text-center row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System by BSCS-3A</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
